b25284f98c9f5f951152ce8c6fb48f5085633263
Kasymov Marsel <marselkasymov23@gmail.com>
2023-01-18 15:02:40 +0600

    Fix the scroll on GameScreen

--- App.js
+++ App.js
@@ -44,7 +44,7 @@ export default function App() {
     screen = <GameOverScreen onRestart={startNewGame} roundsNumber={rounds} userNumber={pickedNumber}/> 
   }
   return (
-    <Pressable onLayout={onLayoutRootView}  style = {styles.rootScreen} onPress={Keyboard.dismiss}>
+    
     <LinearGradient
       colors={[colors.primary700,colors.accent500]}
        style = {styles.rootScreen}>
@@ -63,7 +63,6 @@ export default function App() {
       </ImageBackground>
     
     </LinearGradient>
-    </Pressable>
   );
 }
 
